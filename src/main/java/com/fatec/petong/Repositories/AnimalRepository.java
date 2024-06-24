cf9525a7b94f10dac20dc66f92b7a02c447a8805
edukitito <edukititodu@gmail.com>
2024-06-24 17:42:19 -0300

    Modificações de lógica de animais

--- src/main/java/com/fatec/petong/Repositories/AnimalRepository.java
+++ src/main/java/com/fatec/petong/Repositories/AnimalRepository.java
@@ -25,10 +25,11 @@ public interface AnimalRepository extends JpaRepository<Animais, Integer> {
     @Query(value = "SELECT a.animal_id, a.nome, a.raca, a.descricao, a.idade, a.tipo, a.sexo, a.imagem, a.proprietario_Tipo, a.proprietario_Id FROM animais a inner join ONGs o on a.proprietario_Id=o.ongid where o.cidade = :cidade and a.tipo =:tipo and a.ativo = 1", nativeQuery = true)
     List<Animais> findAnimaisByCidadeTipo(@Param("cidade") String cidade, @Param("tipo") String tipo);
 
-    @Query(value = "SELECT a.animal_id, a.nome, a.raca, a.descricao, a.idade, a.tipo, a.sexo, a.imagem, a.proprietario_Tipo, a.proprietario_Id FROM animais a WHERE a.tipo = :tipo and a.ativo = 1", nativeQuery = true)
+    @Query(value = "SELECT a.animal_id, a.nome, a.raca, a.descricao, a.idade, a.tipo, a.sexo, a.imagem, a.proprietario_Tipo, a.proprietario_Id FROM vw_animais_ativos a WHERE a.tipo = :tipo", nativeQuery = true)
     List<Animais> findAnimaisByTipo(@Param("tipo") String tipo);
 
-    List<Animais> findAnimaisByProprietarioIdAndAndProprietarioTipo(Integer ongId, String proprietarioTipo);
+    @Query(value = "SELECT a.animal_id, a.nome, a.raca, a.descricao, a.idade, a.tipo, a.sexo, a.imagem, a.proprietario_Tipo, a.proprietario_Id FROM vw_animais_ativos a where a.proprietario_Id =:ongId and a.proprietario_Tipo =:proprietario", nativeQuery = true)
+    List<Animais> findAnimaisByProprietarioIdAndAndProprietarioTipo(@Param("ongId") Integer ongId ,@Param("proprietario") String proprietarioTipo);
 
     @Query(value = "SELECT * FROM vw_animais_ativos", nativeQuery = true)
     List<Animais> findAllAtivos();
